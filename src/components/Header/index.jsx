e91a0f38d398d129ed09cc51b22438eefc824e8e
samuel <samuelibiapinolima@gmail.com>
2020-06-11 00:12:04 -0300

    adiçao do botao de ver estoque: funcional

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,6 +3,11 @@ import Nav from '../Nav';
 import './styles.css';
 
 const Header = () => {
+
+  function myScroll() {
+    window.scrollTo(10, 700)
+  }
+
   return (
     <header className="Header">
       <Nav />
@@ -10,7 +15,7 @@ const Header = () => {
         <div className="Header__box">
           <h3 className="Header__name">Fashionista</h3>
           <h4 className="Header__description">Descontos de até 50%</h4>
-          <button className="Header__btn">Ver Estoque</button>
+          <button className="Header__btn" onClick={myScroll}>Ver Estoque</button>
         </div>
       </div>
     </header>
